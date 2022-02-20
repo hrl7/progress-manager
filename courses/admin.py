364c43fa989648620d8ae9844927db6459102a75
zigen <zigen@horol.org>
2022-02-20 20:36:06 +0900

    add black and apply format

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 from .models import Course, Challenge, Step, StepProgress
 
-admin.site.register(Course)
-admin.site.register(Challenge)
-admin.site.register(Step)
-admin.site.register(StepProgress)
 
+class ChallengeInline(admin.TabularInline):
+    model = Challenge
+    extra = 1
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "author")
+    inlines = [ChallengeInline]
+
+
+admin.site.register(Course, CourseAdmin)
+
+
+class StepInline(admin.TabularInline):
+    model = Step
+    extra = 1
+
+
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ("title", "order", "course")
+    inlines = [StepInline]
+
+
+admin.site.register(Challenge, ChallengeAdmin)
+
+
+class StepAdmin(admin.ModelAdmin):
+    list_display = ("title", "order", "challenge")
+
+
+admin.site.register(Step, StepAdmin)
+admin.site.register(StepProgress)
--- courses/models.py
+++ courses/models.py
@@ -1,21 +1,25 @@
 from asyncio import constants
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Course(models.Model):
-    title = models.CharField(max_length=512,unique=True, null=False)
-    created_at = models.DateTimeField("date created")
-    updated_at = models.DateTimeField("date updated",null=True)
-    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=512, unique=True)
+    created_at = models.DateTimeField("date created", default=timezone.now)
+    updated_at = models.DateTimeField("date updated", null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f"{self.title}" 
+        return f"{self.title}"
+
 
 class Challenge(models.Model):
-    title = models.CharField(max_length=512, null=False)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False)
-    description = models.TextField(null=False)
-    created_at = models.DateTimeField("date created")
-    updated_at = models.DateTimeField("date updated",null=True)
+    title = models.CharField(max_length=512)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    description = models.TextField(default="")
+    created_at = models.DateTimeField("date created", default=timezone.now)
+    updated_at = models.DateTimeField("date updated", null=True)
     order = models.IntegerField()
 
     def __str__(self):
@@ -24,58 +28,52 @@ class Challenge(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["course", "title"],
-                name="challenge_title_unique"
-            ),   
-             models.UniqueConstraint(
-                fields=["course", "order"],
-                name="challenge_order_unique"
+                fields=["course", "title"], name="challenge_title_unique"
+            ),
+            models.UniqueConstraint(
+                fields=["course", "order"], name="challenge_order_unique"
             ),
         ]
+
 
 class Step(models.Model):
-    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, null=False)
-    title = models.CharField(max_length=1024, null=False)
-    body = models.TextField(null=True)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1024)
+    body = models.TextField(default="")
     order = models.IntegerField()
-    created_at = models.DateTimeField("date created")
-    updated_at = models.DateTimeField("date updated",null=True) 
+    created_at = models.DateTimeField("date created", default=timezone.now)
+    updated_at = models.DateTimeField("date updated", null=True)
 
-    
     def __str__(self):
         return f"{self.title}"
 
     class Meta:
         constraints = [
+            models.UniqueConstraint(fields=["challenge", "order"], name="step_unique"),
             models.UniqueConstraint(
-                fields=["challenge", "order"],
-                name="step_unique"
-            ),
-            models.UniqueConstraint(
-                fields=["challenge", "title"],
-                name="step_title_unique"
+                fields=["challenge", "title"], name="step_title_unique"
             ),
         ]
+
 
 class StepProgress(models.Model):
     STEP_STATUS = [
-        ('TODO', 'Todo'),
-        ('DOING', 'Doing'),
-        ('DONE', 'Done'),
-        ('GAVE_UP', 'Gave up'),
-        ('STUCKED',  'Stucked'),
-        ('PENDING', 'Pending')
+        ("TODO", "Todo"),
+        ("DOING", "Doing"),
+        ("DONE", "Done"),
+        ("GAVE_UP", "Gave up"),
+        ("STUCKED", "Stucked"),
+        ("PENDING", "Pending"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     step = models.ForeignKey(Step, on_delete=models.CASCADE, null=False)
     state = models.CharField(max_length=20, choices=STEP_STATUS, null=False)
-    created_at = models.DateTimeField("date created")
-    updated_at = models.DateTimeField("date updated",null=True) 
-     
+    created_at = models.DateTimeField("date created", default=timezone.now)
+    updated_at = models.DateTimeField("date updated", null=True)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "step"],
-                name="progress_step_user_unique"
+                fields=["user", "step"], name="progress_step_user_unique"
             ),
         ]
